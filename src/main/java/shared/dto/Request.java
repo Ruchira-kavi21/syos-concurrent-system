5a13f7091289b3de4a5cc7c929725f8bcdf45038
Ruchira Kaveesha <ruchirakaveesha2020@gmail.com>
2026-05-18 12:14:24 +0530

    Create DTO classes

---
+++ src/main/java/shared/dto/Request.java
@@ -0,0 +1,19 @@
+package shared.dto;
+
+import java.io.Serializable;
+
+public class Request implements Serializable {
+    private String action;
+    private String data;
+
+    public Request (String action, String data){
+        this.action = action;
+        this.data = data;
+    }
+    public String getAction(){
+        return action;
+    }
+    public String getData(){
+        return data;
+    }
+}
---
+++ src/main/java/shared/dto/Response.java
@@ -0,0 +1,19 @@
+package shared.dto;
+
+import java.io.Serializable;
+
+public class Response implements Serializable {
+    private String status;
+    private String message;
+
+    public Response (String status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+    public String getStatus() {
+        return status;
+    }
+    public String getMessage() {
+        return message;
+    }
+}
