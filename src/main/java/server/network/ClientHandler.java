b73b99cef31478bc0438b529081c6e67d941e3f9
Ruchira Kaveesha <ruchirakaveesha2020@gmail.com>
2026-05-18 11:11:26 +0530

    server concurrency

--- src/main/java/server/network/ClientHandler.java
+++ src/main/java/server/network/ClientHandler.java
@@ -1,4 +1,34 @@
 package server.network;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
 
-public class ClientHandler {
+public class ClientHandler implements Runnable{
+
+    private final Socket clientSocket;
+    public ClientHandler(Socket socket) {
+        this.clientSocket = socket;
+    }
+
+    @Override
+    public void run(){
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+            PrintWriter writer = new PrintWriter(clientSocket.getOutputStream(), true);
+
+            //read message from client
+            String request = reader.readLine();
+            System.out.println("Client request: " + request);
+
+            //temporary response
+            writer.println("Server received: " + request);
+
+            clientSocket.close();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/server/network/ServerMain.java
+++ src/main/java/server/network/ServerMain.java
@@ -1,4 +1,32 @@
 package server.network;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class ServerMain {
+    private static final int PORT = 5000;
+
+    // thread pool to handle multiple client connections concurrently
+    private static final ExecutorService threadpool = Executors.newFixedThreadPool(10);
+
+    public static void main(String[] args) {
+        System.out.println("Syos Server is starting...");
+
+        try (ServerSocket  serverSocket = new ServerSocket(PORT)) {
+            System.out.println("Server running on port " + PORT);
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("New client connected: " + clientSocket.getInetAddress());
+
+                // handle each client connection in a separate thread
+                threadpool.execute(new ClientHandler(clientSocket));
+            }
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+    }
 }
